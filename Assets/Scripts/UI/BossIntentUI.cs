1e21fc5002f86fcdf628cf849cd309f761d85481
Gabriel <gabrielfuentes7312@gmail.com>
2026-05-17 15:30:15 -0400

    refactor: restore World Space Canvas BossIntentUI and remove HUD intent logic

---
+++ Assets/Scripts/UI/BossIntentUI.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class BossIntentUI : MonoBehaviour
+{
+    [Header("UI Components")]
+    [SerializeField] private TMP_Text intentValueText;
+    [SerializeField] private Image intentIcon;
+    [SerializeField] private TMP_FontAsset intentFont;
+
+    [Header("Icons")]
+    [SerializeField] private Sprite lightAttackIcon;
+    [SerializeField] private Sprite heavyAttackIcon;
+    [SerializeField] private Sprite restIcon;
+
+    private void OnEnable()
+    {
+        BossAI.OnIntentDecided += UpdateIntentUI;
+    }
+
+    private void OnDisable()
+    {
+        BossAI.OnIntentDecided -= UpdateIntentUI;
+    }
+
+    private void UpdateIntentUI(EnemyIntent intent)
+    {
+        int damageValue = 0;
+        Sprite iconToUse = null;
+
+        switch (intent)
+        {
+            case EnemyIntent.LightAttack:
+                damageValue = 6;
+                iconToUse = lightAttackIcon;
+                break;
+            case EnemyIntent.HeavyAttack:
+                damageValue = 12;
+                iconToUse = heavyAttackIcon;
+                break;
+            case EnemyIntent.Rest:
+                damageValue = 0;
+                iconToUse = restIcon;
+                break;
+        }
+
+        if (intentValueText != null)
+        {
+            intentValueText.text = damageValue > 0 ? damageValue.ToString() : "Zzz";
+            if (intentFont != null) intentValueText.font = intentFont;
+        }
+
+        if (intentIcon != null && iconToUse != null)
+        {
+            if (intentIcon.sprite != iconToUse)
+            {
+                intentIcon.sprite = iconToUse;
+                intentIcon.transform.DOPunchScale(Vector3.one * 0.3f, 0.4f, 10, 1);
+            }
+        }
+    }
+}
